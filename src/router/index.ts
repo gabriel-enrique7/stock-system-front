434c845c367d3d329e82eb5d7b4c04682d45372e
Gabriel Enrique <gabrielgeraldo297@gmail.com>
2022-07-03 17:50:41 -0300

    Login page done

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,19 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 import Register from '@/views/Register.vue';
+import Login from '@/views/Login.vue';
 
 const routes: RouteRecordRaw[] = [
     {
         path: '/register',
         name: 'Register',
         component: Register
+    },
+
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
     }
 ]
 
